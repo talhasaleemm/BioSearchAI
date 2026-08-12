e5c1a4e3a9448ab0fdc72a1b58da3fba33661a5f
Talha Saleem <talhasaleem13613@gmail.com>
2026-08-12 17:37:32 +0500

    Support RUN_AS_WORKER in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ set -e
 echo "Running Alembic migrations..."
 alembic upgrade head
 
-if [ $# -eq 0 ]; then
+if [ "$RUN_AS_WORKER" = "true" ]; then
+    echo "Starting Celery worker..."
+    exec celery -A app.tasks.celery_app worker --loglevel=info --concurrency=2
+elif [ $# -eq 0 ]; then
     echo "Starting Uvicorn..."
     exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
 else
